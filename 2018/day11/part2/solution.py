33bfc791119cc2c2ca5ebe7d858ba2f54d9d8908
Jacob S. Pfister <jspfister@sep.com>
2018-12-11 16:44:46 -0500

    Start on solution with better performance

--- 2018/day11/part2/solution.py
+++ 2018/day11/part2/solution.py
@@ -2,25 +2,34 @@ import sys
 
 GRID_SIZE = 300
         
-def populate_single_cells(serial_number):
-    power_by_square = {}
-    for x in range(1, GRID_SIZE + 1):
-        for y in range(1, GRID_SIZE + 1):
-            rack_id = x + 10 
-            power = (rack_id * y + serial_number) * rack_id
-            power = int(str(power)[-3]) - 5
-            power_by_square[x, y, 1] = power
-    return power_by_square
-    
-def populate_rows(grid):
-    power_by_row = {}
-    for row in range(1, GRID_SIZE + 1):
+class PowerMap:
+    def __init__(self, grid_size, serial_number):
+        self.grid_size = grid_size
+        self.data = [[[] for y in range(0, grid_size)] for x in range(0, grid_size)]
+        for x in range(1, grid_size + 1):
+            for y in range(1, grid_size + 1):
+                rack_id = x + 10
+                power = (rack_id * y + serial_number) * rack_id
+                power = int(str(power)[-3]) - 5
+                self.data[x - 1][y - 1].append(power)
+                
+    def calc(self):
+        power_by_rect = self.calc_power_of_all_retangles_rooted_at_origin()
+                
+    def calc_power_of_all_retangles_rooted_at_origin(self):
+        power_by_rect = [[0 for y in range(0, self.grid_size)] for x in range(0, self.grid_size)]
         for size in range(1, GRID_SIZE + 1):
             if size == 1:
-                power_by_row[row, size] = grid[1, row, 1]
-            else:
-                power_by_row[row, size] = power_by_row[row, size - 1] + grid[size, row, 1]
-    return power_by_row
+                power_by_rect[
+                
+    def get_power(self, x, y, size):
+        return self.data[x - 1][y - 1][size]
+                
+    def set_power(self, x, y, size):
+        assert(len(self.data[x - 1][y - 1]) == size - 1)
+        self.data[x - 1][y - 1].append(size)
+    
+power_map = PowerMap(GRID_SIZE, int(sys.argv[1]))
     
 power_by_square = populate_single_cells(int(sys.argv[1]))
 power_by_row = populate_rows(power_by_square)

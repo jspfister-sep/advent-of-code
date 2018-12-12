47357122057e6c5734cecceef3d4acb7e1182c0b
Jacob S. Pfister <jspfister@sep.com>
2018-12-12 18:41:52 -0500

    Polish Day 12

--- 2018/day12/part2/solution.py
+++ 2018/day12/part2/solution.py
@@ -7,11 +7,27 @@ class Garden:
         self.rule_size = len(next(iter(rules)))
         self.offset = 0
         
+    def add_empty_pot_padding(self):
+        padding = (self.rule_size - 1) * '.'
+        self.pots = padding + self.pots + padding
+        self.offset -= len(padding)
+        
+    def grow_to_generation(self, generation):
+        last_garden = self.pots
+        last_offset = self.offset
+        for g in range(1, generation + 1):
+            self.iterate()
+            if self.pots == last_garden:
+                offset_change = self.offset - last_offset
+                print('Generation {} is the same as the previous one but with an offset change of {:+}'.format(g, offset_change))
+                self.offset += (generation - g) * offset_change
+                break
+            else:
+                last_garden = self.pots
+                last_offset = self.offset
+        
     def iterate(self):
-        pots_to_prepend = self.pots_to_pad(self.pots.index('#'))
-        self.pots = pots_to_prepend + self.pots
-        self.offset -= len(pots_to_prepend)
-        self.pots += self.pots_to_pad(len(self.pots) - self.pots.rindex('#') - 1)
+        self.add_empty_pot_padding()
         new_pots = '.' * int(self.rule_size / 2)
         for i in range(int(self.rule_size / 2), len(self.pots) - int(self.rule_size / 2)):
             if self.pots[i - 2:i + 3] in self.rules:
@@ -19,14 +35,18 @@ class Garden:
             else:
                 new_pots += '.'
         self.pots = new_pots
+        self.remove_empty_pot_padding()
+        
+    def remove_empty_pot_padding(self):
         self.offset += len(self.pots) - len(self.pots.lstrip('.'))
         self.pots = self.pots.strip('.')
-
-    def pots_to_pad(self, num_empty_pots):
-        if num_empty_pots < self.rule_size - 1:
-            return '.' * int(self.rule_size - num_empty_pots - 1)
-        else:
-            return ''
+        
+    def sum(self):
+        sum = 0
+        for i in range(0, len(self.pots)):
+            if self.pots[i] == '#':
+                sum += i + self.offset
+        return sum
             
     def __str__(self):
         return self.pots
@@ -47,24 +67,6 @@ with open(sys.argv[1], 'r') as file:
     pots, rules = read_plant_info(file)
     
 garden = Garden(pots, rules)
-
-num_generations = int(sys.argv[2])
-last_garden = garden.pots
-last_offset = garden.offset
-for g in range(1, num_generations + 1):
-    garden.iterate()
-    if garden.pots == last_garden:
-        offset_change = garden.offset - last_offset
-        print('Generation {} repeats the previous generation with a change in offset of {:+}'.format(g, offset_change))
-        garden.offset += (num_generations - g) * offset_change
-        break
-    else:
-        last_garden = garden.pots
-        last_offset = garden.offset
-    
-pot_sum = 0
-for i in range(0, len(garden.pots)):
-    if garden.pots[i] == '#':
-        pot_sum += i + garden.offset
+garden.grow_to_generation(int(sys.argv[2]))
         
-print('The sum of the indexes of all pots with plants is {}'.format(pot_sum))
+print(garden.sum())

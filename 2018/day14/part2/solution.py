41228b704452f6cf38e5b5b93b291d8f6d39d36d
Jacob S. Pfister <jspfister@sep.com>
2018-12-14 02:56:42 -0500

    Deque approach

--- 2018/day14/part2/solution.py
+++ 2018/day14/part2/solution.py
@@ -1,31 +1,34 @@
-import sys
-
-NUM_NEXT_SCORES = 10
+import collections, itertools, sys
 
 def add_new_recipes(recipes, elf1_idx, elf2_idx):
     sum = int(recipes[elf1_idx]) + int(recipes[elf2_idx])
     for digit in str(sum):
-        recipes += digit
-    return recipes
+        recipes.append(digit)
 
 def get_next_recipe(recipes, elf_idx):
     return (elf_idx + int(recipes[elf_idx]) + 1) % len(recipes)
     
 def check_for_scores(recipes, scores_to_find):
-    search_length = min(len(recipes), len(scores_to_find) * 2 - 1)
-    return recipes.rfind(scores_to_find, -search_length)
+    search_length = min(len(recipes), len(scores_to_find) + 1)
+    search_start = len(recipes) - search_length
+    scores_str = ''.join(list(itertools.islice(recipes, search_start, len(recipes))))
+    found_idx = scores_str.find(scores_to_find) 
+    if found_idx >= 0:
+        return len(recipes) - search_length + found_idx
+    return -1
 
-recipes = '37'
+recipes = collections.deque(['3', '7'], 10000000)
 scores_to_find = sys.argv[1]
 elf1_idx = 0
 elf2_idx = 1
 
 while True: 
-    recipes = add_new_recipes(recipes, elf1_idx, elf2_idx)
+    add_new_recipes(recipes, elf1_idx, elf2_idx)
     num_recipes = check_for_scores(recipes, scores_to_find)
     if num_recipes >= 0:
         break
     elf1_idx = get_next_recipe(recipes, elf1_idx)
     elf2_idx = get_next_recipe(recipes, elf2_idx)
+    print(len(recipes), end='\r')
 
 print(num_recipes)
